47177cb6876058d844d863468258909dc495a3df
Daniel <danielbou@outlook.com.br>
2022-04-24 23:25:52 -0300

    Configuração da stack topico e service01

--- curso_aws_cdk/src/main/java/com/myorg/Service01Stack.java
+++ curso_aws_cdk/src/main/java/com/myorg/Service01Stack.java
@@ -5,6 +5,7 @@ import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
@@ -13,11 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Service01Stack extends Stack {
-    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
-        this(scope, id, null, cluster);
+    public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventstopic) {
+        this(scope, id, null, cluster, productEventstopic);
     }
 
-    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productEventstopic) {
         super(scope, id, props);
 
         Map<String, String> envVariables = new HashMap<>();
@@ -25,6 +26,8 @@ public class Service01Stack extends Stack {
                 + ":3306/aws_project01?createDatabaseIfNotExist=true");
         envVariables.put("SPRING_DATASOURCE_USERNAME", "admin");
         envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
+        envVariables.put("AWS_REGION","us-east-1");
+        envVariables.put("AWS_SNS_TOPIC_PRODUCT_EVENTS_ARN", productEventstopic.getTopic().getTopicArn());
 
         ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
                 .serviceName("service-01")
@@ -36,7 +39,7 @@ public class Service01Stack extends Stack {
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
                                 .containerName("aws_project01")
-                                .image(ContainerImage.fromRegistry("danielbou/curso_aws_project01:1.2.0"))
+                                .image(ContainerImage.fromRegistry("danielbou/curso_aws_project01:1.3.0"))
                                 .containerPort(8080)
                                 .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                                 .logGroup(LogGroup.Builder.create(this,"Service01LogGroup")
@@ -67,5 +70,7 @@ public class Service01Stack extends Stack {
                 .scaleOutCooldown(Duration.seconds(60))
                 .build());
 
+        productEventstopic.getTopic()
+                .grantPublish(service01.getTaskDefinition().getTaskRole());
     }
 }
